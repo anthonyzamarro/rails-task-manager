4b63ec4f2cae19e48db6841403a1bdd03c4fca25
Anthony Zamarro <anthony.zamarro@gmail.com>
2017-11-28 14:39:28 +0100

    rails-task-manager complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tasks", to:"tasks#index"
+  get "tasks", to:"tasks#index"
 
-  get "/tasks/:id", to: "tasks#show"
+  get "tasks/new", to: "tasks#new", as: "new_task"
 
-  get "/tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: "task"
 
-  post "/tasks", to: "tasks#create"
+  post "tasks", to: "tasks#create"
 
-  get "/tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
 
-  patch "/tasks/:id", to: "tasks#update"
+  patch "tasks/:id", to: "tasks#update"
 
-  delete "/tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting to seed"
+
+Task.destroy_all
+
+10.times do |task|
+  task = Task.new(name: Faker::Beer.name, completed: false, details: Faker::Beer.alcohol)
+  p task.name
+  p task.details
+  task.save
+end
+
+puts "Seeding complete"
